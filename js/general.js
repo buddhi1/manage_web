81e947b316cd4ce998791fe53e7568b2f7fa5bef
buddhi1 <buddhiashan8@gmail.com>
2020-07-25 22:00:29 -0500

    verion1.1 ready production

--- js/general.js
+++ js/general.js
@@ -1,11 +1,38 @@
 $(document).ready(function () {
 	$("#teaching-content").hide();
-	$(".show_hide").on("click", function () {
-        $('#teaching-content').slideToggle(900);   	
-	});
-    
+    $(".show_hide").on("click", function () {
+    	toggleSlider('.show_hide', '#teaching-content');
+    });
+
+    $("#teaching-content").on("click", function () {
+    	toggleSlider('.show_hide', '#teaching-content');
+    });
+
 	$("#pub-shrink").hide();
 	$(".show_hide2").on("click", function () {
-        $('#pub-shrink').slideToggle(900);   	
+    	toggleSlider('.show_hide2', '#pub-shrink'); 	
+	});
+
+	$("#pub-shrink").on("click", function () {
+    	toggleSlider('.show_hide', '#pub-shrink');
+    });
+
+	$(".nav-link").on("click", function () {
+		$('.nav-link').removeClass('nav-link-active');
+        $(this).addClass('nav-link-active');   	
+	});
+
+	$(".to-top-icon").on("click", function () {
+		$('.nav-link').removeClass('nav-link-active'); 	
 	});
 });
+
+var toggleSlider = function(clk, chg) {
+	txt = $(clk).text();
+	if (txt == 'Read More') {
+		$(clk).text('Read Less');
+	} else {
+		$(clk).text('Read More');
+	}
+    $(chg).slideToggle(900); 
+}
